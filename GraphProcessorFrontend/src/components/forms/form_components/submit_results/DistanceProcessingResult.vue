c05231ca2575bcd41372becbff58e54d41fb7c12
vibecoderhex <rty.sem@yandex.ru>
2026-04-10 21:09:27 +0300

    deleted alert messages for graph operation error. Added handler error to UserInputVertexField.vue component

--- GraphProcessorFrontend/src/components/forms/form_components/submit_results/DistanceProcessingResult.vue
+++ GraphProcessorFrontend/src/components/forms/form_components/submit_results/DistanceProcessingResult.vue
@@ -35,7 +35,7 @@
 </template>
 
 <script setup lang="ts">
-    import type { IDistanceProcessingRootObject } from "../../../../utils/interfaces.ts"
+    import type { IDistanceProcessingRootObject } from "../../../../utils/interfacesAndTypes.ts"
     
     const props = defineProps<IDistanceProcessingRootObject>();
 </script>
